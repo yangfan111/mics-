b2f93d2eff69732bf7da1660d7b6dc9fcc5eaf67
fan yang <yfzc1526@outlook.com>
2018-11-23 17:42:56 +0800

    1

---
+++ push.bat
@@ -0,0 +1,8 @@
+
+cd pure_code_practise && git ps
+cd ../pure_practice && git ps
+cd ../xmind && git ps
+cd ../mics && git ps
+
+
+pause
---
+++ pushAll.bat
@@ -0,0 +1,9 @@
+
+cd pure_code_practise && git ps
+cd ../pure_practice && git ps
+cd ../xmind && git ps
+cd ../mics && git ps
+
+cd ../Reference && git ps
+cd ../kbengine && git ps
+pause
